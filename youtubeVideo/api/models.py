d3f13a4661537ff84a119cda69fa188f9c79eb61
rijusougata13 <rijusougata13@gmail.com>
2022-11-03 18:46:21 +0530

    :sparkles: add api app to settings and create model for video

--- youtubeVideo/api/models.py
+++ youtubeVideo/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Model for storing the video details
+
+class YoutubeVideos(models.Model):
+    video_id = models.CharField(null=False,blank=False, max_length=100)
+    title = models.CharField(null=True,blank=True,max_length=100)
+    description = models.CharField(null=True,blank=True,max_length=100)
+    thumbnail_urls = models.URLField()
+    published_at = models.DateTimeField()
+    channel_id = models.CharField(null=False,blank=False,max_length=100)
+    channel_title = models.CharField(null=True,blank=True,max_length=100)
+    created = models.DateTimeField(
+        auto_now_add=True,
+        null=True,
+        blank=True,
+    )
